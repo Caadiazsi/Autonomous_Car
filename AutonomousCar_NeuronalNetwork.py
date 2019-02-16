9e908be70d0406fd51630558f054615c093e7dd1
Camilo Diaz <caadiazsi@unal.edu.co>
2019-02-16 16:40:17 -0500

    Adding colide points

--- AutonomousCar_NeuronalNetwork.py
+++ AutonomousCar_NeuronalNetwork.py
@@ -6,16 +6,21 @@ print("Hello Cruel World...")
 
 #CAR SETTINGS
 INIT_X = 48
-INIT_Y = 64
+INIT_Y = 48
 WIDTH = 32
-HEIGHT = 64
+HEIGHT = 32
 vel = 5
 rot = 0
 rot_speed = 5
 BLACK = (0,0,0)
-RED = (255,0,0)
 LIGHTGREY = (100,100,100)
-
+RED = (255,0,0)
+GREEN = (0,255,0) 
+BLUE = (0,0,255)
+WHITE = (255,255,255)
+colide_markers = ((0,0),(0,0),(0,0),(0,0),(0,0))
+COL_MULTI = (2,1,0,-1,-2)
+COLIDE_CIRCLES_RADIUS = 5
 #WINDOW SETTINGS
 WIN_WIDTH = 768
 WIN_HEIGHT = 768
@@ -30,7 +35,7 @@ GRIDHEIGHT = WIN_HEIGHT / TILESIZE
 # CAR "IMAGE"
 image_car = py.Surface((WIDTH,HEIGHT))
 image_car.set_colorkey(BLACK)
-image_car.fill(RED)
+image_car.fill(BLUE)
 image_copy = image_car.copy()
 image_copy.set_colorkey(BLACK)
 rect = image_copy.get_rect()
@@ -41,18 +46,22 @@ loop = True
 while loop:
 	CLOCK.tick(FPS)
 	WIN.fill(BLACK)
+	#COPY_CAR_POSITION
+	old_center = rect.center
 	#DRAW_GRID
 	for x_grid in range( 0, WIN_WIDTH, TILESIZE):
 		py.draw.line(WIN, LIGHTGREY, (x_grid,0),(x_grid, WIN_HEIGHT))
 	for y_grid in range ( 0, WIN_HEIGHT, TILESIZE):
 		py.draw.line(WIN, LIGHTGREY, (0,y_grid), ( WIN_WIDTH, y_grid))
+	#DRAW_COLIDE_MARKERS
+	for colide_mar in range(0,5):
+		temp_rot = math.radians(rot+(COL_MULTI[colide_mar]*45))
+		py.draw.circle(WIN, GREEN, (int(old_center[0] + (TILESIZE * 1.5 * math.sin(temp_rot))) ,int(old_center[1] + (TILESIZE * 1.5 * math.cos(temp_rot)))), COLIDE_CIRCLES_RADIUS)
 	#CLOSE_WINDOW
 	for event in py.event.get():
 		if event.type == py.QUIT:
 			loop = False
 	keys = py.key.get_pressed()
-	#COPY_CAR
-	old_center = rect.center
 	#RESET
 	if keys[py.K_r]:
 		old_center = (INIT_X,INIT_Y)
