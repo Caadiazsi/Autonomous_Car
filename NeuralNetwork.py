49b8b088b63520739cee3238c8e544a0487f1849
alejandro-santamaria-perez <alejandro.santamaria.perez@ibm.com>
2019-02-23 20:24:54 -0500

    base

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,105 +1,53 @@
+import keras
 import numpy as np
-from scipy.stats import truncnorm
+import random
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.optimizers import Adam
 
-@np.vectorize
-def sigmoid(x):
-    return 1 / (1 + np.e ** -x)
-activation_function = sigmoid
+class DeepQLearning():
 
-def truncated_normal(mean=0, sd=1, low=0, upp=10):
-    return truncnorm(
-        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
-class NeuralNetwork:
-        
-    
-    def __init__(self, 
-                 no_of_in_nodes, 
-                 no_of_out_nodes, 
-                 no_of_hidden_nodes,
-                 learning_rate,
-                 bias=None
-                ):  
-        self.no_of_in_nodes = no_of_in_nodes
-        self.no_of_out_nodes = no_of_out_nodes
-        
-        self.no_of_hidden_nodes = no_of_hidden_nodes
-            
-        self.learning_rate = learning_rate 
-        self.bias = bias
-        self.create_weight_matrices()
-    
-        
-    
-    def create_weight_matrices(self):
-        """ A method to initialize the weight matrices of the neural 
-        network with optional bias nodes"""
-        
-        bias_node = 1 if self.bias else 0
-        
-        rad = 1 / np.sqrt(self.no_of_in_nodes + bias_node)
-        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
-        self.weights_in_hidden = X.rvs((self.no_of_hidden_nodes, 
-                                       self.no_of_in_nodes + bias_node))
-        rad = 1 / np.sqrt(self.no_of_hidden_nodes + bias_node)
-        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
-        self.weights_hidden_out = X.rvs((self.no_of_out_nodes, 
-                                        self.no_of_hidden_nodes + bias_node))
-        
-        
-        
-    def train(self, input_vector, target_vector):
-        # input_vector and target_vector can be tuple, list or ndarray
-        
-        bias_node = 1 if self.bias else 0
-        if self.bias:
-            # adding bias node to the end of the inpuy_vector
-            input_vector = np.concatenate( (input_vector, [self.bias]) )
-                                    
-            
-        input_vector = np.array(input_vector, ndmin=2).T
-        target_vector = np.array(target_vector, ndmin=2).T
-        
-        output_vector1 = np.dot(self.weights_in_hidden, input_vector)
-        output_vector_hidden = activation_function(output_vector1)
-        
-        if self.bias:
-            output_vector_hidden = np.concatenate( (output_vector_hidden, [[self.bias]]) )
-        
-        
-        output_vector2 = np.dot(self.weights_hidden_out, output_vector_hidden)
-        output_vector_network = activation_function(output_vector2)
-        
-        output_errors = target_vector - output_vector_network
-        # update the weights:
-        tmp = output_errors * output_vector_network * (1.0 - output_vector_network)     
-        tmp = self.learning_rate  * np.dot(tmp, output_vector_hidden.T)
-        self.weights_hidden_out += tmp
-        # calculate hidden errors:
-        hidden_errors = np.dot(self.weights_hidden_out.T, output_errors)
-        # update the weights:
-        tmp = hidden_errors * output_vector_hidden * (1.0 - output_vector_hidden)
-        if self.bias:
-            x = np.dot(tmp, input_vector.T)[:-1,:]     # ???? last element cut off, ???
+    def __init__(self):
+        self.memory = []
+        self.red = self.RedNeural()
+        print(np.argmax(self.red.predict(np.array([1,1,1,1]))[0]))
+
+    def RedNeural(self):
+        model = Sequential()
+        # Input Layer de tamaño 4 (3 sensores), y dos capas ocultas
+        model.add(Dense(24, input_dim=1, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(2, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam())
+        return model
+
+    def recordar(self, estado, accion, recompensa, siguienteEstado, final):
+        self.memory.append((estado, accion, recompensa, siguienteEstado, final))
+
+    def aprender(self):
+        if(len(self.memory))>200:
+            for estado, accion, recompensa, siguienteEstado, final in self.memory:
+                target = recompensa
+                if not final:
+                  target = recompensa + 0.9 * \
+                           np.amax(self.red.predict(siguienteEstado)[0])
+                target_f = self.red.predict(estado)
+                target_f[0][accion] = target
+                self.red.fit(estado, target_f, epochs=1, verbose=0)
         else:
-            x = np.dot(tmp, input_vector.T)
-        self.weights_in_hidden += self.learning_rate * x
-        
-       
-    
-    def run(self, input_vector):
-        # input_vector can be tuple, list or ndarray
-        
-        if self.bias:
-            # adding bias node to the end of the inpuy_vector
-            input_vector = np.concatenate( (input_vector, [1]) )
-        input_vector = np.array(input_vector, ndmin=2).T
-        output_vector = np.dot(self.weights_in_hidden, input_vector)
-        output_vector = activation_function(output_vector)
-        
-        if self.bias:
-            output_vector = np.concatenate( (output_vector, [[1]]) )
-            
-        output_vector = np.dot(self.weights_hidden_out, output_vector)
-        output_vector = activation_function(output_vector)
-    
-        return output_vector
+            minibatch = random.sample(self.memory, 200)
+            for estado, accion, recompensa, siguienteEstado, final in minibatch:
+                target = recompensa
+                if not final:
+                  target = recompensa + 0.9 * \
+                           np.amax(self.red.predict(siguienteEstado)[0])
+                target_f = self.red.predict(estado)
+                target_f[0][accion] = target
+                self.red.fit(estado, target_f, epochs=1, verbose=0)
+
+    def actuar(self, state):
+        act_values = self.red.predict(state)
+        return np.argmax(act_values[0])  # returns action
+
+lolo = DeepQLearning()
